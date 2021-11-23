0bea9a1a7ab0a50bbfeb5577999c92c7d0704729
Miguel <miguelandradebarreto2@gmail.com>
2021-11-23 12:03:16 -0300

    :tada: hello world

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String str = "Hello World";
+        int num = 0;
+
+        System.out.println(str + " - " + num);
+        System.out.printf("%s - %d", str, num);
+    }
+}
