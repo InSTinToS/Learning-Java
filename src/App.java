7dd57ede6057be64df1af7edcb3dd248f4520c78
Miguel <miguelandradebarreto2@gmail.com>
2021-11-23 12:42:46 -0300

    :rocket: array

--- src/App.java
+++ src/App.java
@@ -1,18 +1,37 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        int arraySize = 5;
+        int[] array = new int[arraySize];
+        int[] defaultSettedArray = { 0, 3, 4, 1, 2, 5 };
+
+        System.out.println("enter 5 numbers");
+
         Scanner keyboard = new Scanner(System.in);
 
-        int n1 = 0;
-        int n2 = 0;
-
-        System.out.println("enter two numbers");
-        n1 = keyboard.nextInt();
-        n2 = keyboard.nextInt();
-
-        System.out.println("the sum is: " + (n1 + n2));
+        for (int i = 0; i < array.length; i++)
+            array[i] = keyboard.nextInt();
 
         keyboard.close();
+
+        System.out.println("----------------------");
+
+        // foreach
+        for (int arrayValue : array)
+            System.out.println(arrayValue);
+
+        System.out.println("----------- Sort -----------");
+
+        Arrays.sort(defaultSettedArray);
+
+        for (int arrayValue : defaultSettedArray)
+            System.out.println(arrayValue);
+
+        System.out.println("----------- IndexOf -----------");
+
+        System.out.println(Arrays.binarySearch(defaultSettedArray, 3));
+        System.out.println(Arrays.binarySearch(defaultSettedArray, 9));
     }
 }
