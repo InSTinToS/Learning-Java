fb8928ddcdcd615483497bc031e85385d6e587bb
Miguel <miguelandradebarreto2@gmail.com>
2021-11-23 12:20:52 -0300

    :rocket: Scanner

--- src/App.java
+++ src/App.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        String str = "Hello World";
-        int num = 0;
+        Scanner keyboard = new Scanner(System.in);
 
-        System.out.println(str + " - " + num);
-        System.out.printf("%s - %d", str, num);
+        int n1 = 0;
+        int n2 = 0;
+
+        System.out.println("enter two numbers");
+        n1 = keyboard.nextInt();
+        n2 = keyboard.nextInt();
+
+        System.out.println("the sum is: " + (n1 + n2));
+
+        keyboard.close();
     }
 }
